0b8aaa6c65eda75973b4e1acad9a8e551433365b
Noah ABRAHAM <noahabraham214@gmail.com>
2025-08-24 23:57:54 +0200

    Finishing projects' section

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+//Global params
+
+//Displaying projects
+let projectsContainer = document.getElementById("cards-container");
+
+//Manipulating JSON
+fetch("data/projects.json")
+    .then((response) => response.json())
+    .then((data) => {
+        data.projects.forEach((project) => {
+            const card = document.createElement("div");
+            card.classList.add("project-card");
+            card.innerHTML = `
+                <a href="${project.url}" target="_blank" rel="noopener noreferrer">
+                    <div class="thumbnail-container">
+                        <img src="${project.image_url}" alt="${project.name}">
+                    </div>
+                    <div class="desc-container">
+                        <h3>${project.name}</h3>
+                        <p>${project.description}</p>
+                    </div>
+                </a>`;
+            // <span>${project.tags.join(", ")}</span>;
+            projectsContainer.appendChild(card);
+        });
+    })
+    .catch((error) => console.error("Erreur de chargement :", error));
+
+//Dynamic date
 const myBirthday = new Date("April 20, 2002");
 
 let date = new Date();
